bd606e1ee1bf41164784e55acc64397298374900
ethan-cao <ethancaocgx@gmail.com>
2020-03-15 16:30:27 +0000

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
+import SELECTORS from "../styles/abstracts/_selectors.scss";
 
 const TITLE = "Flickr Image Finder";
-const SEARCH_PLACEHOLDER = "Search Image";
+const SEARCH_PLACEHOLDER = "Search"; 
 
 class Navbar extends React.Component {
 	render() {
 		return (
-			<div className="navbar">
-				<span>{TITLE}</span>
-				<input type="text" placeholder={SEARCH_PLACEHOLDER}></input>
+			<div className={SELECTORS.NAVBAR}>
+				<span className={SELECTORS.TITLE}>{TITLE}</span>
+				<form className={SELECTORS.SEARCH_BOX} action="">
+					<button type="submit"><i class="material-icons">search</i></button>
+					<input type="text" placeholder={SEARCH_PLACEHOLDER} name="search"></input>
+				</form>
 			</div>
 		);
 	}
